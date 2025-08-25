5e6c8579bcd0df92e58ce2a6dc2b7419fe2947e7
AmarMuric <113850369+AmarMuric04@users.noreply.github.com>
2025-08-25 16:10:26 +0200

    refactor: Moves all console method restrictions to one rule (#4)

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,23 @@ import type { ESLint, Linter } from "eslint";
 
 import { recommended } from "./configs/recommended.js";
 import * as BooleanNamingConvention from "./rules/boolean-naming-convention.js";
-import * as NoConsoleLog from "./rules/no-console-log.js";
-import * as NoConsoleWarn from "./rules/no-console-warn.js";
-import * as NoConsoleError from "./rules/no-console.error.js";
 import * as NoHandlerSuffix from "./rules/no-handler-suffix.js";
 import * as NoObjectsInDeps from "./rules/no-objects-in-deps.js";
 import * as ParamsNamingConvention from "./rules/params-naming-convention.js";
 import * as PreferArrowFunctions from "./rules/prefer-arrow-functions.js";
 import * as PreferReactFc from "./rules/prefer-react-fc.js";
 import * as PreferReactWithHooks from "./rules/prefer-react-with-hooks.js";
+import * as RestrictConsoleMethods from "./rules/restrict-console-methods.js";
 
 const rules = {
   [BooleanNamingConvention.name]: BooleanNamingConvention.rule,
-  [NoConsoleError.name]: NoConsoleError.rule,
-  [NoConsoleLog.name]: NoConsoleLog.rule,
-  [NoConsoleWarn.name]: NoConsoleWarn.rule,
   [NoHandlerSuffix.name]: NoHandlerSuffix.rule,
   [NoObjectsInDeps.name]: NoObjectsInDeps.rule,
   [ParamsNamingConvention.name]: ParamsNamingConvention.rule,
   [PreferArrowFunctions.name]: PreferArrowFunctions.rule,
   [PreferReactFc.name]: PreferReactFc.rule,
   [PreferReactWithHooks.name]: PreferReactWithHooks.rule,
+  [RestrictConsoleMethods.name]: RestrictConsoleMethods.rule,
 };
 
 export type RuleKey = keyof typeof rules;
