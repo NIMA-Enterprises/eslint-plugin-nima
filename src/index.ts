4e28afc1482277b1e05988bf01d0b340b0e0d4f3
AmarMuric <113850369+AmarMuric04@users.noreply.github.com>
2025-08-26 20:36:28 +0200

    feat: Adds new manage-functions rule & Updates project setup (#7)

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-import type { RuleModule } from "@typescript-eslint/utils/ts-eslint";
+import type {
+  FlatConfig,
+  RuleModule,
+} from "@typescript-eslint/utils/ts-eslint";
 import type { ESLint, Linter } from "eslint";
 
-import { recommended } from "./configs/recommended.js";
-import * as BooleanNamingConvention from "./rules/boolean-naming-convention.js";
-import * as NoHandlerSuffix from "./rules/no-handler-suffix.js";
-import * as NoObjectsInDeps from "./rules/no-objects-in-deps.js";
-import * as ParamsNamingConvention from "./rules/params-naming-convention.js";
-import * as PreferArrowFunctions from "./rules/prefer-arrow-functions.js";
-import * as PreferReactFc from "./rules/prefer-react-fc.js";
-import * as PreferReactWithHooks from "./rules/prefer-react-with-hooks.js";
-import * as RestrictConsoleMethods from "./rules/restrict-console-methods.js";
+import * as BooleanNamingConvention from "./rules/boolean-naming-convention";
+import * as ManageFunctions from "./rules/manage-functions";
+import * as NoHandlerSuffix from "./rules/no-handler-suffix";
+import * as NoObjectsInDeps from "./rules/no-objects-in-deps";
+import * as ParamsNamingConvention from "./rules/params-naming-convention";
+import * as PreferArrowFunctions from "./rules/prefer-arrow-functions";
+import * as PreferReactFc from "./rules/prefer-react-fc";
+import * as PreferReactWithHooks from "./rules/prefer-react-with-hooks";
+import * as RestrictConsoleMethods from "./rules/restrict-console-methods";
 
 const rules = {
   [BooleanNamingConvention.name]: BooleanNamingConvention.rule,
+  [ManageFunctions.name]: ManageFunctions.rule,
   [NoHandlerSuffix.name]: NoHandlerSuffix.rule,
   [NoObjectsInDeps.name]: NoObjectsInDeps.rule,
   [ParamsNamingConvention.name]: ParamsNamingConvention.rule,
@@ -22,8 +26,6 @@ const rules = {
   [RestrictConsoleMethods.name]: RestrictConsoleMethods.rule,
 };
 
-export type RuleKey = keyof typeof rules;
-
 interface Plugin extends Omit<ESLint.Plugin, "rules"> {
   configs: {
     "flat/recommended": Linter.Config;
@@ -32,27 +34,37 @@ interface Plugin extends Omit<ESLint.Plugin, "rules"> {
   rules: Record<RuleKey, RuleModule<any, any, any>>;
 }
 
+type RuleKey = keyof typeof rules;
+
 const plugin = {
   configs: {} as Plugin["configs"],
 
   meta: {
-    name: "eslint-plugin-dragonswap",
+    name: "eslint-plugin-nima",
   },
 
   rules,
 };
 
+const recommended: {
+  [K in RuleKey as `nima/${K}`]?: FlatConfig.RuleLevel;
+} = {
+  "nima/no-handler-suffix": "error",
+  "nima/prefer-react-fc": "warn",
+  "nima/restrict-console-methods": "error",
+};
+
 Object.assign(plugin.configs, {
   "flat/recommended": {
     plugins: {
-      dragonswap: plugin,
+      nima: plugin,
     },
     rules: recommended,
   },
   recommended: {
-    plugins: ["dragonswap"],
+    plugins: ["nima"],
     rules: recommended,
   },
 });
 
-export default plugin;
+export = plugin;
