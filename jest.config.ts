3308c6f34188d658132be90a27fad198b0d21cba
AmarMuric <113850369+AmarMuric04@users.noreply.github.com>
2025-08-27 14:52:04 +0200

    chore: Adds tsc-alias for better DX & Dynamically gets paths from tsconfig in jest.config.mts (#10)

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,13 @@
+import { pathsToModuleNameMapper } from "ts-jest";
+
+import { compilerOptions } from "./tsconfig.json";
+
 module.exports = {
   collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
   moduleFileExtensions: ["ts", "js"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
   preset: "ts-jest",
   testEnvironment: "node",
   transform: {
